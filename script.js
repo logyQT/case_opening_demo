f860d386a6d2589581ec237842b1d07c896e04b6
logyQT <stoltmann96@gmail.com>
2022-10-10 23:36:14 +0200

    .

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-
 const roll_container = $(".roll-container");
-
+const inventory_container = $(".inventory");
 const words = [
     "colossal",
     "giddy",
@@ -106,32 +105,50 @@ const words = [
     "efficacious",
 ];
 
+var inventory = [];
+
 function add_items(preview = false) {
     let item_count = 40;
+    let winning_item;
     if (preview) item_count = 5;
     for (let i = 0; i < item_count; i++) {
-        let div = document.createElement("div");
-
-        div.classList.add("item");
-        let img = document.createElement("img");
-
-        img.src = "https://picsum.photos/150?" + Math.floor(Math.random() * 100);
-        let text = document.createElement("p");
-        text.innerText = words[Math.floor(Math.random() * 100)];
-        div.appendChild(img);
-        div.appendChild(text);
-        roll_container.appendChild(div);
+        let item = generate_item();
+        roll_container.appendChild(item.div);
+        if (i == 22) winning_item = item;
     }
+    return winning_item;
+}
+
+function update_inventory(x) {
+    inventory_container.appendChild(x.div.cloneNode("deep"));
+    inventory_container.scroll(0, inventory_container.scrollHeight);
+}
+
+function generate_item() {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    let text = document.createElement("p");
+
+    let item_name = words[Math.floor(Math.random() * 100)];
+    let img_src = "https://picsum.photos/150?" + item_name;
+
+    div.classList.add("item");
+    img.src = img_src;
+    text.innerText = item_name;
+
+    div.appendChild(img);
+    div.appendChild(text);
+
+    return { div: div, img: img_src, name: item_name };
 }
 
 function generate_roll_offset() {
     let offset = Math.floor(Math.random() * (75 - -75) - 75);
     document.documentElement.style.setProperty("--roll_offset", offset + "px");
-    console.log(offset);
 }
 
 function clear() {
-    let items = $$(".item");
+    let items = $(".roll-container").querySelectorAll(".item");
     items.forEach((e) => {
         e.remove();
     });
@@ -140,7 +157,7 @@ function clear() {
 function roll() {
     if (roll_container.getAttribute("state") == "rolling") return;
     clear();
-    add_items();
+    winning_item = add_items();
     generate_roll_offset();
 
     roll_container.setAttribute("state", "rolling");
@@ -148,6 +165,8 @@ function roll() {
         "animationend",
         () => {
             roll_container.setAttribute("state", "rolled");
+            inventory.push(winning_item);
+            update_inventory(winning_item);
         },
         { once: true }
     );
